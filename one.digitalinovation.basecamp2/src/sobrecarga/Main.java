0bbba62632e80478e81370ea276f61dbf406c5f0
EzequielCarlosCorrea <ezequielcarloscorrea@hotmail.com>
2022-01-24 14:06:39 -0300

    Sobrecarga métodos java

---
+++ one.digitalinovation.basecamp2/src/sobrecarga/Main.java
@@ -0,0 +1,15 @@
+package sobrecarga;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Quadrilátero
+        System.out.println("\n");
+        System.out.println("Quadrilátero");
+        Quadriláteros.area(3);
+        Quadriláteros.area(5d,5d);
+        Quadriláteros.area(7,8,9);
+        Quadriláteros.area(5f,5f);
+    }
+}
